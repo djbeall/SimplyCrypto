2ef1770fd45ffafe5cdd79a7beb21b90ea065284
Daniel Beall <djbeall1101@gmail.com>
2019-04-23 18:27:17 -0400

    added table view

--- SimplyCrypto/ViewController.swift
+++ SimplyCrypto/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CryptoCell: UITableViewCell {
+    @IBOutlet weak var coinName: UILabel!
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2 // change later
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath)
+        if let myCell = cell as? CryptoCell {
+            myCell.layer.borderWidth = 0.5
+            myCell.coinName.text = String(indexPath.row)
+        }
+        return cell;
     }
 
 
